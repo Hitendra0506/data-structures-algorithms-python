249e050196172c96c2385e062ab97f6baa593cf8
Hitendra Singh <hitendrasingh0506@gmail.com>
2024-11-24 13:42:05 +0530

    added LinkedList.py

---
+++ data-structures/linked-list/LinkedList.py
@@ -0,0 +1,44 @@
+class Node:
+
+    def __init__(self, value):
+        
+        self.value = value
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        
+        new_node = Node(value)
+
+        self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
+
+    def prepend(self, value):
+
+        new_node = Node(value)
+
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+
+    def print_list(self):
+
+        curr = self.head
+
+        while(curr is not None):
+            print(curr.value)
+            curr = curr.next
+
+    def make_empty(self):
+        
+        self.head = None
+        self.tail = None
+        self.length = 0
