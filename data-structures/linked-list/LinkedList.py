42f978c1431940f6797a82bac937c24c1c0df63e
Hitendra Singh <hitendrasingh0506@gmail.com>
2024-11-24 15:23:24 +0530

    added pop

--- data-structures/linked-list/LinkedList.py
+++ data-structures/linked-list/LinkedList.py
@@ -17,28 +17,61 @@ class LinkedList:
         
         new_node = Node(value)
 
-        self.tail.next = new_node
-        self.tail = new_node
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        
         self.length += 1
 
     def prepend(self, value):
 
         new_node = Node(value)
-
-        new_node.next = self.head
-        self.head = new_node
+        
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        
         self.length += 1
 
     def print_list(self):
+        vals = []
 
         curr = self.head
-
         while curr is not None:
-            print(curr.value)
+            vals.append(curr.value)
             curr = curr.next
+        
+        print(' -> '.join(vals))
 
     def make_empty(self):
         
         self.head = None
         self.tail = None
         self.length = 0
+
+    def pop(self):
+        if self.tail is None:
+            return None
+            
+        curr = self.head
+        while curr.next != self.tail:
+            curr = curr.next
+        
+        popped_node = self.tail
+        curr.next = None
+        self.tail = curr
+        
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        
+        return popped_node
+
