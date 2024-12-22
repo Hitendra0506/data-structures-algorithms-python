3cb1a9e6f21646e5b9a18d72396a1392dae61d6f
Hitendra Singh <hitendrasingh0506@gmail.com>
2024-12-22 10:48:52 +0530

    DS for BST and HT

---
+++ data-structures/binary-search-tree/BinarySearchTree.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        curr = self.root
+        while curr:
+            if value == curr.value:
+                return True
+            curr = curr.left if value < curr.value else curr.right
+        return False
